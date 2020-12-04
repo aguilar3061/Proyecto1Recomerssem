4062a5a19d56d5abf74828cae12af74742c24ff3
Carlos Aguilar <aguilarcepnet@gmail.com>
2020-12-04 18:54:37 +0100

    Iconos/Ofertas

--- RECOMENSSEM/PAGINAS/OFERTAS/ofertas.php
+++ RECOMENSSEM/PAGINAS/OFERTAS/ofertas.php
@@ -1,86 +1,97 @@
 
 
+<?php
+    require_once("..\..\php_librarys\bd.php");
+	$listaOfertas = selectOferta();
+
+	$admin = true;
+	
+?>
+
+
+
 <html>
 	<head>
 	
 		<!-- <link rel="stylesheet" href="style/stilos.css"> -->
 		<!-- <script src="js/script.js"></script> -->
-
-		<link rel="stylesheet" href="/BOOTSTRAP/style/bootstrap.min.css">
+		<link rel="stylesheet" href="../../style/bootstrap.min.css">
+		<link rel="stylesheet" href="../../style/ofertas.css">
 
 
        <!-- ICONOS -->
-	   <link href="../fontawesome-free-5.15.1-web/fontawesome-free-5.15.1-web/css/all.css" rel="stylesheet"> <!--load all styles -->
+	   <link href="../../FONTAWESOME/fontawesome-free-5.15.1-web/css/all.css" rel="stylesheet"> <!--load all styles -->
 
 
 	</head>
 	<body>
 
 				  
-		
-	<?php include "../php_partrials/menu.php"; ?>  
       
-	  <br>
-	  <div class="mr-1 row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5" style="margin: 2px;">
-		  
-		  <?php
-			  foreach ($pokemons as $poke){
-		  ?>
+		<br>
+		<div class="mr-1 row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5" style="margin: 10px;">
+			
+			<?php
+				foreach ($listaOfertas as $oferta){
+			?>
 
-			  <div class="col mb-4">
-				  <div class="card h-100">
-					  <img src="<?php echo $poke["imagen"]?>" >
-					  <div class="card-body">
-				  
-						  <strong><?php echo $poke["numero"]?> - <?php echo $poke["nombre"]?></strong>   
-						  <br>    
-						  <?php 
-							  $listaTipos = selectTipusDePk($poke["id"]);
-							  foreach ($listaTipos as $tipo){
-						  ?>
-							  <span class="badge badge-primary"><?php echo $tipo["nombre"]?> </span>
-						  <?php
-							  }
-						  ?>
+				<div class="col mb-4">
+					<div class="card h-100" >
+						<center>
+							<img src="/RECOMENSSEM/media/IMGoferta.png" width="80%" > 
 
-					  <br> 
-					  </div>
-					  <div class="card-footer badge-secondary ">
+							<div class="card-body">
+								<strong><?php $tienda = selectTiendaConID($oferta["Tienda_idTienda"]); echo $tienda["nombre"] ?></strong>   
+								<br>    
+								<strong><?php echo $oferta["nombre"]?></strong>  
+								<br> 
+							</div>
+							
+						</center> 
+						
+						<?php
+							if($admin == true){
+						?>
+							<div class="card-footer badge-secondary ">
+								<form  action="../php_controllers/pokemonController.php" method="POST">
+									<input value="<?php echo $oferta["idOferta"]?>" type="hidden" name="idOferta">
+									<button class="btn btn-outline-danger" type="submit" name="Eliminar"> <i class="far fa-trash-alt"></i> </button>  
+								</form>
 
-						  <form  action="../php_controllers/pokemonController.php" method="POST">
+								<form action="../pokemon.php" method="POST" >
+									<input value="<?php echo $oferta["idOferta"]?>" type="hidden" name="idOferta" >
+									<button class="btn btn-outline-primary" type="submit" name="Update"> <i class="far fa-edit"></i> </button>
+								</form>  
+							</div>
+						<?php
+							}
+						?>
+					</div>
+				</div>
+			<?php
+				}
+			?>
 
-							  <input value="<?php echo $poke["imagen"]?>" type="hidden" name="rutaIMG">
-							  <input value="<?php echo $poke["id"]?>" type="hidden" name="IDpokemon">
-							  <button class="btn btn-outline-danger" type="submit" name="Eliminar"> <i class="far fa-trash-alt"></i> </button>  
-						  </form>
-
-						  <form action="../pokemon.php" method="POST" >
-
-
-							  <input value="<?php echo $poke["id"]?>" type="hidden" name="IDpokemon" >
-							  <button class="btn btn-outline-primary" type="submit" name="Update"> <i class="far fa-edit"></i> </button>
-								
-						  </form>  
-						  
-					  </div>
-
-				  </div>
-			  </div>
-				
-		  <?php
-			  }
-		  ?>
-	  </div>
+		</div>
 
 
 
+	
 
-  <a href="../pokemon.php" class="btn btn-success boton"> + </a>
+	<?php
+		if($admin == true){
+	?>
+		<a class="btn btn-success boton"> + </a>
+	<?php
+		}else{
+	?>	
+		<a class="btn btn-success botonCanjear"> Canjear </a>
+	<?php	
+		}
+	?>
+
+	
 			   
-
-
-
-
 
 		
 	</body>
--- RECOMENSSEM/php_librarys/bd.php
+++ RECOMENSSEM/php_librarys/bd.php
@@ -6,7 +6,7 @@
         $password = "mysql";
         
 
-        $conexion = new PDO("mysql:host=$servername;dbname=pokedex", $username, $password);
+        $conexion = new PDO("mysql:host=$servername;dbname=db_recomencem", $username, $password);
         // set the PDO error mode to exception
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conexion->exec("set names utf8");
@@ -18,6 +18,42 @@
     function closeBD(){
         return null;
     }
+
+
+    function selectOferta(){
+        $conexion = openBD();
+
+        $sentenciaText = "
+        select * from oferta
+        ";
+        $sentencia =$conexion->prepare($sentenciaText);
+        $sentencia->execute();
+        $resultado = $sentencia->fetchAll();
+
+        $conexion = closeBD();
+
+        return $resultado;
+    }
+
+    function selectTiendaConID($idTienda){
+   
+        $conexion = openBD();
+
+        $sentenciaText = 
+        "
+        SELECT * FROM tienda where idTienda=:idTienda
+        ;";
+
+        $sentencia =$conexion->prepare($sentenciaText);
+        $sentencia->bindParam(":idTienda",    $idTienda);
+        $sentencia->execute();
+        $resultado = $sentencia->fetch();
+
+        $conexion = closeBD();
+
+        return $resultado;
+    }
+
 
 
 
