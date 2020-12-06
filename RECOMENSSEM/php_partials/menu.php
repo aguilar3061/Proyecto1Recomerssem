67dfd71690b057568381b4aea01f4f33ef224f62
horitzon1999 <horitzon@gmail.com>
2020-12-06 16:09:20 +0100

    Quiza se lia
    Quiza se lia

---
+++ RECOMENSSEM/php_partials/menu.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-primary">
+    <img src="/RECOMENSSEM/media/logo-ninot-small.png" alt="logo" width="100" height="45">
+
+ 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" 
+  >
+    <ul class="navbar-nav mr-auto">
+    </ul>
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="navbar-brand"  href="">Mercat del ninot</a>
+      </li>
+    </ul>
+  
+    <form class="form-inline my-2 my-lg-0">
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-user"></i>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="" id="iniciarSesion">Iniciar sesion</a>
+          <a class="dropdown-item" href="" id="Registrarse">Registrarse</a>
+        </div>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+      <a href=""></a>
+    </ul>
+    <ul class="navbar-nav">
+      <a href=""></a>
+    </ul>
+    </form>
+  </div>
+</nav>
