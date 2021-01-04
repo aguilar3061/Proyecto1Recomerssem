f3ddec9269e54fe47ea6999d220c6bcce49a6ab9
Carlos Aguilar <aguilarcepnet@gmail.com>
2021-01-04 15:05:12 +0100

    juego carlos

--- RECOMENSSEM/PAGINAS/JUEGOS/CARLOS/index.php
+++ RECOMENSSEM/PAGINAS/JUEGOS/CARLOS/index.php
@@ -2,12 +2,18 @@
 
 <html>
 	<head>
-	
+		 <?php 
+			include_once  $_SERVER['DOCUMENT_ROOT'] . '/RECOMENSSEM/php_partials/menu.php' ;
+
+		?> 
+
+<link rel="stylesheet" href="/RECOMENSSEM/style/bootstrap.min.css">
+<link href="/RECOMENSSEM/FONTAWESOME/fontawesome-free-5.15.1-web/css/all.css" rel="stylesheet"> 
+	   
+
 
 		<title>Pagina Carlos Aguilar</title>
-		
 		<link rel="stylesheet" href="style/stilos.css">
-
 	
 	</head>
 
@@ -60,6 +66,7 @@
 		</div> 
 		
 		<div id="juego">
+			<div id="h">
 			<section id="cajonEstanteria">
 			</section>
 			
@@ -165,7 +172,8 @@
 
 
 
-
+			</div>
+		
 		</div>
 		
 		
@@ -201,6 +209,10 @@
 		
 		
 	</body>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
 
 		<script src="./js/script.js"></script>
 </html>
